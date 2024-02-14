4f3995ff80923b0d1115f2682125a52ad9213a80
sabbirHossain <sabbirshnt@gmail.com>
2024-02-14 18:42:38 +0600

    inplay updated

--- src/components/ui/BetTable/BetTable.jsx
+++ src/components/ui/BetTable/BetTable.jsx
@@ -6,119 +6,126 @@ const BetTable = ({ data, keys }) => {
   const navigateGameList = (keys) => {
     navigate(`/game-details/${data[keys]?.eventTypeId}/${keys}`);
   };
-// console.log(data);
+  // console.log(data);
   return (
     <>
-      <div  
+      {data[keys]?.inPlay === 0 && (
+        <div
           onClick={() => navigateGameList(keys)}
-      _ngcontent-ng-c943649379="" className="table-item ng-star-inserted">
-        <div _ngcontent-ng-c943649379="" className="teamlist-info">
-          <div _ngcontent-ng-c943649379="" className="match-time">
-            {data[keys]?.inPlay === 1 ? (
-              <span
-                _ngcontent-ng-c943649379=""
-                className="inplay ng-star-inserted"
-              >
-                In-play
-              </span>
-            ) : (
+          _ngcontent-ng-c943649379=""
+          className="table-item ng-star-inserted"
+        >
+          <div _ngcontent-ng-c943649379="" className="teamlist-info">
+            <div _ngcontent-ng-c943649379="" className="match-time">
+              {/* {data[keys]?.inPlay === 1 ? (
+            <span
+              _ngcontent-ng-c943649379=""
+              className="inplay ng-star-inserted"
+            >
+              In-play
+            </span>
+          ) : (
+            <p _ngcontent-ng-c943649379="" className=" ng-star-inserted">
+              {data[keys]?.date}
+            </p>
+          )} */}
               <p _ngcontent-ng-c943649379="" className=" ng-star-inserted">
                 {data[keys]?.date}
               </p>
-            )}
-          </div>
-          <h3 _ngcontent-ng-c943649379="" className="team-title">
-            <p
-              _ngcontent-ng-c943649379=""
-              className="ng-star-inserted"
-              style={{ display: "flex", gap: "5px" }}
-            >
-              {/*  <span
+            </div>
+            <h3 _ngcontent-ng-c943649379="" className="team-title">
+              <p
                 _ngcontent-ng-c943649379=""
-                role="img"
-                className="mat-icon notranslate material-icons favorite mat-ligature-font mat-icon-no-color ng-star-inserted"
-                aria-hidden="true"
-                data-mat-icon-type="font"
+                className="ng-star-inserted"
+                style={{ display: "flex", gap: "5px" }}
               >
-                  
-              </span> */}
-              {/* <FaStar /> */}
-              <span
-                _ngcontent-ng-c943649379=""
-                className="team-name ng-star-inserted"
-              >
-                {data[keys]?.eventName?.split("v")[0]}
-              </span>
-            </p>
-            <p _ngcontent-ng-c943649379="" className="ng-star-inserted">
-              <span
-                _ngcontent-ng-c943649379=""
-                className="team-name ng-star-inserted"
-              >
-                {data[keys]?.eventName?.split("v")[1]}
-              </span>
-            </p>
-            <p _ngcontent-ng-c943649379="" className="ng-star-inserted"></p>
-          </h3>
-          {/* <div
-            _ngcontent-ng-c943649379=""
-            className="inplay-wrap ng-star-inserted"
-          >
-            <span
+                {/*  <span
               _ngcontent-ng-c943649379=""
               role="img"
-              className="mat-icon notranslate material-icons mat-ligature-font mat-icon-no-color"
+              className="mat-icon notranslate material-icons favorite mat-ligature-font mat-icon-no-color ng-star-inserted"
               aria-hidden="true"
               data-mat-icon-type="font"
             >
-              play_arrow
-            </span>
-          </div> */}
-        </div>
-        <div
+                
+            </span> */}
+                {/* <FaStar /> */}
+                <span
+                  _ngcontent-ng-c943649379=""
+                  className="team-name ng-star-inserted"
+                >
+                  {data[keys]?.eventName?.split("v")[0]}
+                </span>
+              </p>
+              <p _ngcontent-ng-c943649379="" className="ng-star-inserted">
+                <span
+                  _ngcontent-ng-c943649379=""
+                  className="team-name ng-star-inserted"
+                >
+                  {data[keys]?.eventName?.split("v")[1]}
+                </span>
+              </p>
+              <p _ngcontent-ng-c943649379="" className="ng-star-inserted"></p>
+            </h3>
+            {/* <div
           _ngcontent-ng-c943649379=""
-          className="count-v-wrap ng-star-inserted"
+          className="inplay-wrap ng-star-inserted"
         >
-          <button
+          <span
             _ngcontent-ng-c943649379=""
-            mat-flat-button=""
-            className="count-value back-count mdc-button mdc-button--unelevated mat-mdc-unelevated-button mat-unthemed mat-mdc-button-base"
-            mat-ripple-loader-uninitialized=""
-            mat-ripple-loader-class-name="mat-mdc-button-ripple"
+            role="img"
+            className="mat-icon notranslate material-icons mat-ligature-font mat-icon-no-color"
+            aria-hidden="true"
+            data-mat-icon-type="font"
           >
-            <span className="mat-mdc-button-persistent-ripple mdc-button__ripple"></span>
-            <span className="mdc-button__label">
-              <h4 _ngcontent-ng-c943649379="">
-                {data[keys]?.[0]?.ex?.availableToBack[0]?.price}
-              </h4>
-              <p _ngcontent-ng-c943649379="">
-              {data[keys]?.[0]?.ex?.availableToBack[0]?.size}
-              </p>{" "}
-            </span>
-            <span className="mat-mdc-focus-indicator"></span>
-            <span className="mat-mdc-button-touch-target"></span>
-          </button>
-          <button
+            play_arrow
+          </span>
+        </div> */}
+          </div>
+          <div
             _ngcontent-ng-c943649379=""
-            mat-flat-button=""
-            className="count-value lay-count mdc-button mdc-button--unelevated mat-mdc-unelevated-button mat-unthemed mat-mdc-button-base"
-            mat-ripple-loader-uninitialized=""
-            mat-ripple-loader-class-name="mat-mdc-button-ripple"
+            className="count-v-wrap ng-star-inserted"
           >
-            <span className="mat-mdc-button-persistent-ripple mdc-button__ripple"></span>
-            <span className="mdc-button__label">
-              <h4 _ngcontent-ng-c943649379="">
-              {data[keys]?.[0]?.ex?.availableToLay[0]?.price}
-              </h4>
-              <p _ngcontent-ng-c943649379="">
-              {data[keys]?.[0]?.ex?.availableToLay[0]?.size}
-              </p>{" "}
-            </span>
-            <span className="mat-mdc-focus-indicator"></span>
-            <span className="mat-mdc-button-touch-target"></span>
-          </button>
+            <button
+              _ngcontent-ng-c943649379=""
+              mat-flat-button=""
+              className="count-value back-count mdc-button mdc-button--unelevated mat-mdc-unelevated-button mat-unthemed mat-mdc-button-base"
+              mat-ripple-loader-uninitialized=""
+              mat-ripple-loader-class-name="mat-mdc-button-ripple"
+            >
+              <span className="mat-mdc-button-persistent-ripple mdc-button__ripple"></span>
+              <span className="mdc-button__label">
+                <h4 _ngcontent-ng-c943649379="">
+                  {data[keys]?.[0]?.ex?.availableToBack[0]?.price}
+                </h4>
+                <p _ngcontent-ng-c943649379="">
+                  {data[keys]?.[0]?.ex?.availableToBack[0]?.size}
+                </p>{" "}
+              </span>
+              <span className="mat-mdc-focus-indicator"></span>
+              <span className="mat-mdc-button-touch-target"></span>
+            </button>
+            <button
+              _ngcontent-ng-c943649379=""
+              mat-flat-button=""
+              className="count-value lay-count mdc-button mdc-button--unelevated mat-mdc-unelevated-button mat-unthemed mat-mdc-button-base"
+              mat-ripple-loader-uninitialized=""
+              mat-ripple-loader-class-name="mat-mdc-button-ripple"
+            >
+              <span className="mat-mdc-button-persistent-ripple mdc-button__ripple"></span>
+              <span className="mdc-button__label">
+                <h4 _ngcontent-ng-c943649379="">
+                  {data[keys]?.[0]?.ex?.availableToLay[0]?.price}
+                </h4>
+                <p _ngcontent-ng-c943649379="">
+                  {data[keys]?.[0]?.ex?.availableToLay[0]?.size}
+                </p>{" "}
+              </span>
+              <span className="mat-mdc-focus-indicator"></span>
+              <span className="mat-mdc-button-touch-target"></span>
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
